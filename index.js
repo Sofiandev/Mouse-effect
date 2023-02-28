620f073cdedef66b762f99355d6e9b3da8ca9c4b
SofianAcDev <sofian.achbabi@gmail.com>
2023-02-28 20:07:40 +0100

    +style & js factorisation

--- index.js
+++ index.js
@@ -2,20 +2,25 @@
 
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
 
-// S'assurer que les liens sont clickables
+// S'assurer que les liens sont cliquables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
 
-const mouse = document.querySelector(".mouse");
-const cursor = document.querySelector("#cursor");
-const mouse1 = document.querySelector("#mouse1");
-const mouse2 = document.querySelector("#mouse2");
+const mouses = document.querySelectorAll(".mouse");
+// const cursor = document.querySelector("#cursor");
+// const mouse1 = document.querySelector("#mouse1");
+// const mouse2 = document.querySelector("#mouse2");
 
 window.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.pageX + "px";
-  cursor.style.top = e.pageY + "px";
-  mouse1.style.left = e.pageX + "px";
-  mouse1.style.top = e.pageY + "px";
-  mouse2.style.left = e.pageX + "px";
-  mouse2.style.top = e.pageY + "px";
+  // cursor.style.left = e.x + "px";
+  // cursor.style.top = e.y + "px";
+  // mouse1.style.left = e.x + "px";
+  // mouse1.style.top = e.y + "px";
+  // mouse2.style.left = e.x + "px";
+  // mouse2.style.top = e.y + "px";
+
+  mouses.forEach((mouse) => {
+    mouse.style.left = e.x + "px";
+    mouse.style.top = e.y + "px";
+  });
 });
